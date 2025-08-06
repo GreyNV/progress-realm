531c75768cae46bc7059b00c49dc89e5ba195eec
GreyNV <34779441+GreyNV@users.noreply.github.com>
2025-08-06 15:11:50 +0300

    Refactor character art to dedicated tab element

--- tests/test_character_ui.py
+++ tests/test_character_ui.py
@@ -49,7 +49,7 @@ const elements = {
   'character-slots-left': new Elem('div'),
   'character-slots-right': new Elem('div'),
   'equipment-items': new Elem('div'),
-  'character-image': new Elem('img')
+  'character-image': new Elem('div')
 };
 
 global.document = {
